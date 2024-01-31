72dd7b630314834f08f0028010649df6558fdc44
Jerome MR <jerome@nebula-tales.com>
2024-01-31 19:17:18 +0000

    Set up the enemy attack on the base.

---
+++ Scripts/base.gd
@@ -0,0 +1,4 @@
+extends Node3D
+
+func take_damage()-> void:
+	print("All our bases are belong to them :(...")
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,6 +2,10 @@ extends PathFollow3D
 
 @export var speed: float = 5.1
 
+@onready var base: Node3D = get_tree().get_first_node_in_group("base")
+
 
 func _process(delta: float) -> void:
 	progress += delta * speed
+	if progress_ratio == 1.0:
+		base.take_damage()
