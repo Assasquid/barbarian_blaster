06d3fa20f099d5a0fe7dff2a12b18807784560a6
Jerome MR <jerome@nebula-tales.com>
2024-02-05 16:21:46 +0000

    Change the health variable for a property.

--- Scripts/base.gd
+++ Scripts/base.gd
@@ -2,13 +2,21 @@ extends Node3D
 
 @export var max_health: int = 5
 
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		print("Health was changed.")
+		label_3d.text = str(current_health)
+		if current_health < 1:
+			get_tree().reload_current_scene()
+
 @onready var label_3d: Label3D = $Label3D
 
 
 func _ready()-> void:
-	label_3d.text = str(max_health)
+	current_health = max_health
 
 
 func take_damage()-> void:
-	max_health -= 1
-	label_3d.text = str(max_health)
+	print("Damage dealt to base!")
+	current_health -= 1
