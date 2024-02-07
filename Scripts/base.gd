2f71ee442e74c7cb97cab2eddf9896f18d46c285
Jerome MR <jerome@nebula-tales.com>
2024-02-07 23:30:06 +0000

    Add setter method and improve health display.

--- Scripts/base.gd
+++ Scripts/base.gd
@@ -6,9 +6,15 @@ var current_health: int:
 	set(health_in):
 		current_health = health_in
 		print("Health was changed.")
-		label_3d.text = str(current_health)
+		label_3d.text = str(current_health) + "/" + str(max_health)
+		
+		var red: Color = Color.RED
+		var white: Color = Color.WHITE
+		label_3d.modulate = red.lerp(white, float(current_health) / float(max_health))
+		
 		if current_health < 1:
 			get_tree().reload_current_scene()
+
 
 @onready var label_3d: Label3D = $Label3D
 
