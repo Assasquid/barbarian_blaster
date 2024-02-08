4adc10364d692309aad442dca3724e2aa80adcd3
Jerome MR <jerome@nebula-tales.com>
2024-02-08 21:42:03 +0000

    Detect and change gridmap cells.

--- Scripts/ray_picker_camera.gd
+++ Scripts/ray_picker_camera.gd
@@ -1,12 +1,20 @@
 extends Camera3D
 
-@onready var ray_cast_3d: RayCast3D = $RayCast3D
-
 @export var clickable_range: float = 100.0
+@export var gridmap: GridMap
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
 
 
 func _process(delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * clickable_range
 	ray_cast_3d.force_raycast_update()
-	printt(ray_cast_3d.get_collider(), ray_cast_3d.get_collision_point())
+	
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			var collision_point = ray_cast_3d.get_collision_point()
+			var cell = gridmap.local_to_map(collision_point)
+			if gridmap.get_cell_item(cell) == 0:
+				gridmap.set_cell_item(cell, 1)
