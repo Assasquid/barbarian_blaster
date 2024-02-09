818710e1f6d5dd2cc641f1ff8af93669aec84543
Jerome MR <jerome@nebula-tales.com>
2024-02-09 23:19:07 +0000

    Change colour of a cell on click.

--- Scripts/ray_picker_camera.gd
+++ Scripts/ray_picker_camera.gd
@@ -14,7 +14,8 @@ func _process(delta: float) -> void:
 	if ray_cast_3d.is_colliding():
 		var collider = ray_cast_3d.get_collider()
 		if collider is GridMap:
-			var collision_point = ray_cast_3d.get_collision_point()
-			var cell = gridmap.local_to_map(collision_point)
-			if gridmap.get_cell_item(cell) == 0:
-				gridmap.set_cell_item(cell, 1)
+			if Input.is_action_pressed("click"):
+				var collision_point = ray_cast_3d.get_collision_point()
+				var cell = gridmap.local_to_map(collision_point)
+				if gridmap.get_cell_item(cell) == 0:
+					gridmap.set_cell_item(cell, 1)
