41ee56cfc5d66186846cec09baf6d8840785c762
Jerome MR <jerome@nebula-tales.com>
2024-02-11 21:22:08 +0000

    Change the size of projectiles.

--- Scripts/turret.gd
+++ Scripts/turret.gd
@@ -2,7 +2,14 @@ extends Node3D
 
 @export var projectile: PackedScene
 
+@onready var right_barrel: MeshInstance3D = $TurretBase/TurretTop/Visor/RightBarrel
+@onready var left_barrel: MeshInstance3D = $TurretBase/TurretTop/Visor/LeftBarrel
 
-func _ready() -> void:
-	var shot = projectile.instantiate()
-	add_child(shot)
+
+func _on_timer_timeout() -> void:
+	var shot_right = projectile.instantiate()
+	add_child(shot_right)
+	shot_right.global_position = right_barrel.global_position
+	var shot_left = projectile.instantiate()
+	add_child(shot_left)
+	shot_left.global_position = left_barrel.global_position
