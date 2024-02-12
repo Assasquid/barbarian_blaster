ac45db3eeb43de170327f06e17099383360be797
Jerome MR <jerome@nebula-tales.com>
2024-02-12 00:30:38 +0000

    Allow turrets to shoot and destroy enemies.

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,8 +1,20 @@
 extends PathFollow3D
 
 @export var speed: float = 5.1
+@export var max_health := 50
 
 @onready var base: Node3D = get_tree().get_first_node_in_group("base")
+
+var current_health: int:
+	set(new_value):
+		current_health = new_value
+		
+		if current_health < 1:
+			queue_free()
+
+
+func _ready() -> void:
+	current_health = max_health
 
 
 func _process(delta: float) -> void:
--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -3,6 +3,7 @@ extends Area3D
 var direction := Vector3.FORWARD
 
 @export var speed := 30.0
+@export var damage_amount := 5
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
@@ -10,3 +11,9 @@ func _physics_process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_area_entered(area: Area3D) -> void:
+	if area.is_in_group("enemy_area"):
+		area.get_parent().current_health -= damage_amount
+		queue_free()
