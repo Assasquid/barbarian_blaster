4ebaf57efac801fb140bbad7347a96b1f40cc354
Jerome MR <jerome@nebula-tales.com>
2024-02-12 00:46:39 +0000

    Make the turret shoot the enemy closest to base.

--- Scripts/turret.gd
+++ Scripts/turret.gd
@@ -9,8 +9,9 @@ var enemy_path: Path3D
 
 
 func _physics_process(delta: float) -> void:
-	var enemy = enemy_path.get_children().back()
-	look_at(enemy.global_position, Vector3.UP, true)
+	var enemy = find_best_target()
+	if enemy:
+		look_at(enemy.global_position, Vector3.UP, true)
 
 
 func _on_timer_timeout() -> void:
@@ -18,7 +19,21 @@ func _on_timer_timeout() -> void:
 	add_child(shot_right)
 	shot_right.global_position = right_barrel.global_position
 	shot_right.direction = global_transform.basis.z
+	
 	var shot_left = projectile.instantiate()
 	add_child(shot_left)
 	shot_left.global_position = left_barrel.global_position
 	shot_left.direction = global_transform.basis.z
+
+
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var best_progress = 0
+	
+	for enemy in enemy_path.get_children():
+		if enemy is PathFollow3D:
+			if enemy.progress > best_progress:
+				best_target = enemy
+				best_progress = enemy.progress
+	
+	return best_target
