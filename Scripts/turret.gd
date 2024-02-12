c62249ad795cc44939fa4405ff301fc3bece216d
Jerome MR <jerome@nebula-tales.com>
2024-02-12 01:01:28 +0000

    Add a range to turrets, shoot the best target.
    Best target meaning an enemy that is in range and closest to the base.

--- Scripts/turret.gd
+++ Scripts/turret.gd
@@ -1,29 +1,32 @@
 extends Node3D
 
 @export var projectile: PackedScene
+@export var turret_range := 10.0
 
 var enemy_path: Path3D
+var target: PathFollow3D
 
 @onready var right_barrel: MeshInstance3D = $TurretBase/TurretTop/Visor/RightBarrel
 @onready var left_barrel: MeshInstance3D = $TurretBase/TurretTop/Visor/LeftBarrel
 
 
 func _physics_process(delta: float) -> void:
-	var enemy = find_best_target()
-	if enemy:
-		look_at(enemy.global_position, Vector3.UP, true)
+	target = find_best_target()
+	if target:
+		look_at(target.global_position, Vector3.UP, true)
 
 
 func _on_timer_timeout() -> void:
-	var shot_right = projectile.instantiate()
-	add_child(shot_right)
-	shot_right.global_position = right_barrel.global_position
-	shot_right.direction = global_transform.basis.z
-	
-	var shot_left = projectile.instantiate()
-	add_child(shot_left)
-	shot_left.global_position = left_barrel.global_position
-	shot_left.direction = global_transform.basis.z
+	if target:
+		var shot_right = projectile.instantiate()
+		add_child(shot_right)
+		shot_right.global_position = right_barrel.global_position
+		shot_right.direction = global_transform.basis.z
+		
+		var shot_left = projectile.instantiate()
+		add_child(shot_left)
+		shot_left.global_position = left_barrel.global_position
+		shot_left.direction = global_transform.basis.z
 
 
 func find_best_target() -> PathFollow3D:
@@ -33,7 +36,9 @@ func find_best_target() -> PathFollow3D:
 	for enemy in enemy_path.get_children():
 		if enemy is PathFollow3D:
 			if enemy.progress > best_progress:
-				best_target = enemy
-				best_progress = enemy.progress
+				var distance := global_position.distance_to(enemy.global_position)
+				if distance < turret_range:
+					best_target = enemy
+					best_progress = enemy.progress
 	
 	return best_target
