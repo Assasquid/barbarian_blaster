7e011bb664eaeddc051b6424966a9ce8f467ee44
Jerome MR <jerome@nebula-tales.com>
2024-02-13 00:50:23 +0000

    Try alternate method for handling timer.
    This time the timer is directly in the base_level and its wait_time is initially set to 2 seconds.
    Then the wait_time is changed each time an enemy is instantiated.

--- Scripts/EnemyPath.gd
+++ Scripts/EnemyPath.gd
@@ -2,21 +2,37 @@ extends Path3D
 
 @export var enemy_scene: PackedScene
 @export var timer_min := 1.3
-@export var timer_max := 2.6
+@export var timer_max := 5.0
 
-var timer: Timer
+#var timer: Timer
+@onready var timer: Timer = $Timer
+
 var random_duration
+var enemy_number_debug
 
 func _ready() -> void:
-	timer = Timer.new()
-	add_child(timer)
+	enemy_number_debug = 1
+	#timer = Timer.new()
+	#add_child(timer)
+	print(timer.wait_time)
 	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
 	
-	generate_random_duration()
+	#generate_random_duration()
 	
-	timer.start(random_duration)
-	print("Starting timer with random duration = " + str(random_duration))
+	#timer.start(random_duration)
+	#print("Starting timer with random duration = " + str(random_duration))
 
+
+func _on_timer_timeout() -> void:
+	spawn_enemy()
+	print("Spawning enemy: " + str(enemy_number_debug))
+	generate_random_duration()
+	#timer.start(random_duration)
+	timer.wait_time = random_duration
+	timer.start()
+	print("Next enemy in: " + str(random_duration))
+	enemy_number_debug += 1
 
 func spawn_enemy() -> void:
 	var new_enemy = enemy_scene.instantiate()
@@ -25,10 +41,3 @@ func spawn_enemy() -> void:
 
 func generate_random_duration():
 	random_duration = randf_range(timer_min, timer_max)
-
-
-func _on_timer_timeout() -> void:
-	spawn_enemy()
-	generate_random_duration()
-	timer.start(random_duration)
-	print("REstarting timer with random duration = " + str(random_duration))
