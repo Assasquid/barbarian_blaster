0ac38aa306b27949f9c3ca28bb4d17558766f315
Jerome MR <jerome@nebula-tales.com>
2024-02-13 00:58:54 +0000

    Remove debug code and tweak timer_max.

--- Scripts/EnemyPath.gd
+++ Scripts/EnemyPath.gd
@@ -2,15 +2,13 @@ extends Path3D
 
 @export var enemy_scene: PackedScene
 @export var timer_min := 1.3
-@export var timer_max := 5.0
+@export var timer_max := 2.6
 
 @onready var timer: Timer = $Timer
 
 var random_duration
-var enemy_number_debug
 
 func _ready() -> void:
-	enemy_number_debug = 1
 	print(timer.wait_time)
 	timer.timeout.connect(_on_timer_timeout)
 	timer.start()
@@ -18,12 +16,9 @@ func _ready() -> void:
 
 func _on_timer_timeout() -> void:
 	spawn_enemy()
-	print("Spawning enemy: " + str(enemy_number_debug) + " after having waited: " + str(random_duration))
 	generate_random_duration()
 	timer.wait_time = random_duration
 	timer.start()
-	enemy_number_debug += 1
-	print("Next enemy (enemy " + str(enemy_number_debug) + ") in: " + str(random_duration))
 
 func spawn_enemy() -> void:
 	var new_enemy = enemy_scene.instantiate()
