28b74d20d2882d58b4577a1ca87b2642b32e1e2c
Jerome MR <jerome@nebula-tales.com>
2024-02-14 18:29:03 +0000

    Add a win condition and prepare a victory layer.

--- Scripts/enemy_path.gd
+++ Scripts/enemy_path.gd
@@ -2,6 +2,8 @@ extends Path3D
 
 @export var enemy_scene: PackedScene
 @export var difficulty_manager: Node
+@export var victory_layer: CanvasLayer
+
 
 @onready var timer: Timer = $Timer
 
@@ -22,6 +24,7 @@ func spawn_enemy() -> void:
 	var new_enemy = enemy_scene.instantiate()
 	new_enemy.max_health = difficulty_manager.get_enemy_health()
 	add_child(new_enemy)
+	new_enemy.tree_exited.connect(enemy_defeated)
 
 
 func generate_random_duration():
@@ -32,3 +35,12 @@ func generate_random_duration():
 
 func _on_difficulty_manager_stop_spawning_enemies() -> void:
 	timer.stop()
+
+
+func enemy_defeated() -> void:
+	if timer.is_stopped():
+		for child in get_children():
+			if child is PathFollow3D:
+				return
+		print("You won!")
+		victory_layer.visible = true
