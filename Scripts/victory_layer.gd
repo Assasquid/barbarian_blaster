74718461d3f2496cf5d36a27de125c71d1478aae
Jerome MR <jerome@nebula-tales.com>
2024-02-15 23:16:56 +0000

    Set up new visual assets.

--- Scripts/victory_layer.gd
+++ Scripts/victory_layer.gd
@@ -6,6 +6,7 @@ extends CanvasLayer
 @onready var star_2: TextureRect = %Star2
 @onready var star_3: TextureRect = %Star3
 @onready var health_label: Label = %HealthLabel
+@onready var money_label: Label = %MoneyLabel
 
 @onready var base = get_tree().get_first_node_in_group("base")
 @onready var bank = get_tree().get_first_node_in_group("bank")
@@ -18,8 +19,8 @@ func victory() -> void:
 		health_label.visible = true
 	if bank.money >= money_goal:
 		star_3.modulate = Color.WHITE
+		money_label.visible = true
 		
-
 
 func _on_retry_button_pressed() -> void:
 	get_tree().reload_current_scene()
